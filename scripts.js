9f147537de1256368647b4f418431fd24a5eee34
Nabokov2004 <nabokov2004@gmail.com>
2023-10-23 18:27:08 +0300

    First commit

---
+++ scripts.js
@@ -0,0 +1,42 @@
+var close = document.getElementsByClassName("closeChat");
+var chat = document.getElementsByClassName("chatMessage")
+close[0].addEventListener("click", function () {
+  chat[0].classList.add("hidden");
+
+})
+function showChat(){
+    
+    const chatMessages = document.getElementsByClassName('chat-container');
+    chatMessages[0].classList.remove("hidden");
+}
+
+
+
+
+function sendMessage() {
+    const username = document.getElementById('username').value;
+    const message = document.getElementById('message').value;
+  
+    if (username && message) {
+      const chatMessages = document.getElementById('chat-messages');
+      const messageElement = document.createElement('div');
+      messageElement.innerHTML = `<strong>${username}:</strong> ${message}`;
+      chatMessages.appendChild(messageElement);
+  
+      // Clear the message input
+      document.getElementById('message').value = '';
+  
+      // Save the chat messages to localStorage
+      const messages = chatMessages.innerHTML;
+      localStorage.setItem('chatMessages', messages);
+    }
+  }
+  
+  // Load chat messages from localStorage on page load
+  window.addEventListener('load', () => {
+    const chatMessages = document.getElementById('chat-messages');
+    const savedMessages = localStorage.getItem('chatMessages');
+    if (savedMessages) {
+      chatMessages.innerHTML = savedMessages;
+    }
+  });
